b7ce5fcfb549c8ca822303df15e3c8c3c1c7ba11
tk-neuron <fruche0930@gmail.com>
2022-12-19 16:49:15 +0900

    refactor: calc relative spiketime with np.array (increase in speed)

--- glmcc/util.py
+++ glmcc/util.py
@@ -1,37 +1,9 @@
 """
 useful functions for preprocessing data for GLMCC, and output results
 """
+import numpy as np
 
 
-def spiketime_relative(spiketime_tar, spiketime_ref, window_size=50.0):
-    """
-    calculate relative spike time (target - reference) within a time window
-    :param spiketime_tar: list, target neuron's spiketime [msec]
-    :param spiketime_ref: list, reference neuron's spiketime [msec]
-    :param window_size: default: 50.0, float or int, window size when making CC histogram
-    :return t_sp: relative spiketime
-    """
-
-    t_sp = []
-    min_i, max_i = 0, 0
-
-    for j, tsp_j in enumerate(spiketime_ref):
-        # reuse min_i and max_i values for next iteration to decrease the amount of elements to scan
-        min_i = _search_max_idx(lst=spiketime_tar, upper=tsp_j - window_size, start_idx=min_i)
-        max_i = _search_max_idx(lst=spiketime_tar, upper=tsp_j + window_size, start_idx=max_i)
-
-        # a list of relative spike time
-        t_sp.extend([(spiketime_tar[i] - spiketime_ref[j]) for i in range(min_i, max_i)])
-
-    return t_sp
-
-
-def _search_max_idx(lst, upper, start_idx=0):
-    """
-    returns index of the largest element of all smaller than upper in lst; scans from start_idx
-    note that lst has to be a list of elements monotonously increasing
-    """
-    idx = start_idx
-    while len(lst) > idx and lst[idx] <= upper:
-        idx += 1
-    return idx
+def spiketime_relative(spiketime_i, spiketime_j, window_size=50.0):
+    t_diff = np.array([spiketime_i - t_j for t_j in spiketime_j]).flatten()
+    return t_diff[np.abs(t_diff) <= window_size]
